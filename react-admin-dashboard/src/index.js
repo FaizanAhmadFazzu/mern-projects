2b005736014fac45ce11e50154b18bb0fc64bb1e
Faizan Ahmad Khan <ahmadfaizan1604@gmail.com>
2022-04-26 17:51:36 +0530

    react admin dashboard

---
+++ react-admin-dashboard/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { DarkModeContextProvider } from "./context/darkModeContext"
+
+ReactDOM.render(
+  <DarkModeContextProvider>
+    <App />
+  </DarkModeContextProvider>,
+  document.getElementById('root')
+);
+
